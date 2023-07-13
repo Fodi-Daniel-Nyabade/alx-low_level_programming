3c2df93687c524d08e2767b48c95cc0c72a6589f
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2023-07-13 11:07:06 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a statement using the puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
