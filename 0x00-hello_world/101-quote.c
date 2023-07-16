dc3bb7effc0ed687d8b455f248b7d546a0e79d7b
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2023-07-16 10:46:21 -0700

    unix update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <unistd.h>
+#include <stdio.h>
 /**
- * main - Entry point
+ * main - entry point
  * Return 0 (success)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+		return (0);/*successfully executed */
 }
