c367e3ace28652e6c64ac992f4790cba8f5c838f
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2023-07-16 14:42:06 -0700

    corrections

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 9; num++)
+		for (num2 = num1 + 1; num2 <= 9; num2++)
 		{
 			putchar('0' + num1);
 			putchar('0' + num2);
