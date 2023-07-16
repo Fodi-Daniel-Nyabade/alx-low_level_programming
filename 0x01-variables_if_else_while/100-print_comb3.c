38f65cf01257a5ad21d53f7f4deddd6a1111821a
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2023-07-16 14:32:26 -0700

    print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,16 +11,19 @@ int main(void)
 	int num2;
 
 	for (num1 = 0; num1 < 10; num1++)
-		for (num2 = num1 + 1; num2 < 9; num2++)
 	{
-		putchar((num1 % 10) + '0');
-		putchar((num2 % 10) + '0');
+		for (num2 = num1 + 1; num2 <= 9; num++)
+		{
+			putchar('0' + num1);
+			putchar('0' + num2);
 
-		if (num1 == 8 && num2 == 9)
-			continue;
-		putchar(',');
-		putchar(' ');
+			if (num1 != 8 || num2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
-	return (0);/* shows completeness */
+	return (0);/* indicates successful execution */
 }
