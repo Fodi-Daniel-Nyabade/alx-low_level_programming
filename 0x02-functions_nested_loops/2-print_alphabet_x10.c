7c0dfd717072408aad0cd929ba0132803ad266db
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2023-07-18 08:52:11 -0700

    another update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,12 @@
 
 void print_alphabet_x10(void)
 {
-	for (int limit = 0; limit < 10; limit++)
+	int limit;
+	char letter;
+
+	for (limit = 0; limit < 10; limit++)
 	{
-		for (char letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 	}
 	_putchar('\n');
