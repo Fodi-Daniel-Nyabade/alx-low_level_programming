8908e8138ca21756707ee02b530aba52b8e8ec11
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2023-07-24 14:51:58 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_rev - print string in reverse order
+ * @s: parameter passed to the function
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
