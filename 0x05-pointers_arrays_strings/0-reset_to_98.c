80397df4474985c6345f67d9f1ad540003b211c9
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2023-07-24 09:43:07 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it points to to 98
+ *
+ * @n: - parameter passed to the function
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	if (n != NULL)
+	{
+		*n = 98;
+	}
