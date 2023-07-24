dad35d0af334506571114007ba396ff09558c08e
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2023-07-24 15:37:55 -0700

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_array - print n elements of array
+ * @a: a parameter to the function
+ * @n: a parameter to the function
+ */
+
+void print_array(int *a, int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		printf("%d", a[j]);
+
+		if (j != (n - 1))
+		{
+			printf(",");
+		}
+	}
+	printf("\n");
+}
