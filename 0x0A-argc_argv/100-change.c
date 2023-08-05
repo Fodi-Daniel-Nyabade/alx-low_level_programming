a9ebfdab51fac235475d10d00a14d7f4567a10d8
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2023-08-05 14:09:49 -0700

    updates

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * main - prints the minimum number of coin to 
+ * main - prints the minimum number of coin to
  * make change for an amount of money
  * @argc: count number of arguments
  * @argv: array of arguments
