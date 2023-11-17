19274dba09ac571cdc8b6ccbcf2e9f44bade3f5f
Fodi-Daniel-Nyabade <fodidaniel567@gmail.com>
2023-11-17 03:45:57 -0800

    repo updated

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Compile all .c files into position-independent code
-# and create the dynamic library
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89
-gcc -shared -fPIC -o liball.so *.c
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Fodi-Daniel-Nyabade/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
-export LD-PRELOAD="$PWD/../libhack.so"
